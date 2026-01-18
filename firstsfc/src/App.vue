aaa74a65ef8c62e81666364104e71f71b2dd583d
Iane Sofia Francheska Padua <iapadua@student.apc.edu.ph>
2026-01-18 05:23:39 +0000

    food

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
-<style scoped></style>
+<script></script>
+
+<style></style>
