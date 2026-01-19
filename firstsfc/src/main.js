335ef1450d09735b8166c2a5327de81a32d345cf
Iane Sofia Francheska Padua <iapadua@student.apc.edu.ph>
2026-01-20 02:57:15 +0800

    ?

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
-import FoodItem from './components/FoodItem2.vue'
-
+import FoodItem2 from './components/FoodItem2.vue'
 const app = createApp(App)
+
 app.component('food-item', FoodItem)
-app.component('food-item2', FoodItem2)
+app.component('food-item2', FoodItem2) 
+
 app.mount('#app')
