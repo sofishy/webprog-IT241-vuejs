4afc2262a7490b9ede4c8d281f8f1c08c849f6d8
Iane Sofia Francheska Padua <iapadua@student.apc.edu.ph>
2026-01-29 16:55:57 +0800

    task 3

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,22 +1,12 @@
-<script setup>
-
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
- 
-const instruments = ref([])
- 
-async function getInstruments() {
-  const { data } = await supabase.from('instruments').select()
-  instruments.value = data
-}
- 
-onMounted(() => {
-   getInstruments()
-})
-</script>
- 
 <template>
-  <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
-  </ul>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
+
+<script></script>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,11 +4,15 @@ import App from './App.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 
 app.component('personal-profile', PersonalProfile) 
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2) 
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 
 app.mount('#app')
